aa738f1a94fc7434b9a2e42d09626c718522a88e
小倉輝 <x7deed53bc9k8@gmail.com>
2017-09-02 00:15:00 +0900

    eventのshowとeditを実装

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,4 +3,18 @@ class EventsController < ApplicationController
         @money=Money.find(1)
         @events=Event.all
     end
+    def show
+        @event=Event.find(params[:id])
+    end
+    def edit
+        @event=Event.find(params[:id])
+    end
+    def update
+        @event=Event.find(params[:id])
+        @event.assign_attributes(params[:event].permit(:title, :amount, :day))
+        @event.save
+        @money=Money.find(1)
+        @events=Event.all
+        render :index
+    end
 end
