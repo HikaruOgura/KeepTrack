5c5d2927a85d8991618133c52e929e3801f860a1
小倉輝 <x7deed53bc9k8@gmail.com>
2017-09-02 10:38:43 +0900

    eventの作成と削除を実装

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,18 +3,46 @@ class EventsController < ApplicationController
         @money=Money.find(1)
         @events=Event.all
     end
+
     def show
         @event=Event.find(params[:id])
     end
+
     def edit
         @event=Event.find(params[:id])
     end
+
     def update
         @event=Event.find(params[:id])
         @event.assign_attributes(params[:event].permit(:title, :amount, :day,:recorded))
-        @event.save
-        @money=Money.find(1)
-        @events=Event.all
-        render :index
+        if @event.save
+            @money=Money.find(1)
+            @events=Event.all
+            render :index
+        else
+            render "new"
+        end
+    end
+
+
+    def new
+        @event=Event.new
+    end
+
+    def create
+        @event=Event.new(params[:event].permit(:title, :amount, :day,:recorded))
+        if @event.save
+            @money=Money.find(1)
+            @events=Event.all
+            redirect_to  :root,notice: "新しいeventを登録しました"
+        else
+            render "new"
+        end
+    end
+    def destroy
+        @event=Event.find(params[:id])
+        @event.destroy
+        redirect_to :root,notice: "eventを削除しました"
+
     end
 end
