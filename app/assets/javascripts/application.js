fdd33b2b764947060b1115d0a1011738ea7165a2
小倉輝 <x7deed53bc9k8@gmail.com>
2017-09-10 13:40:55 +0900

    リンク作成

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,19 +15,19 @@
 //= require turbolinks
 //= require_tree .
 
-//modal
+//新規イベント作成モーダル
 $(function(){
 
- $("#create_event").click(function(){
+ $("#create_link").click(function(){
      $("body").append('<div class="modal-bg" id="bg-create"></div>');
      //画面中央を計算する関数を実行
      modalResize();
 
      //モーダルウィンドウを表示
-     $("#bg-create,.modal-main").fadeIn("slow");
+     $("#bg-create,#create").fadeIn("slow");
 
      //クリックしてモーダルを閉じる
-      $("#return").click(function(){
+      $(".return").click(function(){
            $("#create,#bg-create").fadeOut("slow",function(){
           //挿入した<div id="modal-bg"></div>を削除
                $('#bg-create').remove() ;
@@ -54,6 +54,82 @@ $(function(){
 
  });
 
+//所持金修正モーダル
+ $("#modify_link").click(function(){
+     $("body").append('<div class="modal-bg" id="bg-modify"></div>');
+     //画面中央を計算する関数を実行
+     modalResize();
+
+     //モーダルウィンドウを表示
+     $("#bg-modify,#modify").fadeIn("slow");
+
+     //クリックしてモーダルを閉じる
+      $(".return").click(function(){
+           $("#modify,#bg-modify").fadeOut("slow",function(){
+          //挿入した<div id="modal-bg"></div>を削除
+               $('#bg-modify').remove() ;
+          });
+
+         });
+
+     //画面の左上からmodal-mainの横幅・高さを引き、その値を2で割ると画面中央の位置が計算できます
+      $(window).resize(modalResize);
+       function modalResize(){
+
+             var w = $(window).width();
+           var h = $(window).height();
+
+             var cw = $(".modal-main").outerWidth();
+            var ch = $(".modal-main").outerHeight();
+
+         //取得した値をcssに追加する
+             $(".modal-main").css({
+                 "left": ((w - cw)/2) + "px",
+                 "top": ((h - ch)/2) + "px"
+           });
+      }
+
+ });
+
+//かてきょイベント作成モーダル
+ $("#baito_link").click(function(){
+     $("body").append('<div class="modal-bg" id="bg-baito"></div>');
+     //画面中央を計算する関数を実行
+     modalResize();
+
+     //モーダルウィンドウを表示
+     $("#bg-baito,#baito").fadeIn("slow");
+
+     //クリックしてモーダルを閉じる
+      $(".return").click(function(){
+           $("#baito,#bg-baito").fadeOut("slow",function(){
+          //挿入した<div id="modal-bg"></div>を削除
+               $('#bg-baito').remove() ;
+          });
+
+         });
+
+     //画面の左上からmodal-mainの横幅・高さを引き、その値を2で割ると画面中央の位置が計算できます
+      $(window).resize(modalResize);
+       function modalResize(){
+
+             var w = $(window).width();
+           var h = $(window).height();
+
+             var cw = $(".modal-main").outerWidth();
+            var ch = $(".modal-main").outerHeight();
+
+         //取得した値をcssに追加する
+             $(".modal-main").css({
+                 "left": ((w - cw)/2) + "px",
+                 "top": ((h - ch)/2) + "px"
+           });
+      }
+
+ });
+
+
+//最初に出てくるモーダル
 
  $(window).load(function(){
       //body内の最後に<div id="modal-bg"></div>を挿入
